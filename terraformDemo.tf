49769d88790a3202224df2d1f90d57d1eb9d4377
Khushboo Nigam <khushboo2243@gmail.com>
2019-02-27 15:23:30 -0500

    adding the variable for private key

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -4,6 +4,7 @@ variable "fingerprint" {}
 variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
+variable "private_key_path" {}
 
 provider "oci" {
   tenancy_ocid = "${var.tenancy_ocid}"
