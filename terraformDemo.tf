688418d6fe163ee5d6405c300e80d5b77847cdbd
Khushboo Nigam <khushboo2243@gmail.com>
2019-02-27 14:00:31 -0500

    Updating new VCN

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -6,6 +6,16 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 provider "oci" {
-  auth   = "InstancePrincipal"
-  region = "us-ashburn-1"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region = "${var.region}"
+}
+
+resource "oci_core_virtual_network" "VCN" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "VCN"
+  compartment_id = "${var.compartment_ocid}"
+  display_name = "VCN"
 }
