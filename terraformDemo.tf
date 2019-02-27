bc3319cb0741f7b77114e109cdfc2c24fed5b798
Khushboo Nigam <khushboo2243@gmail.com>
2019-02-27 15:04:50 -0500

    Updating the provider oci

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -7,7 +7,10 @@ variable "region" {}
 
 provider "oci" {
   
- auth   = "InstancePrincipal"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
   region = "${var.region}"
 }
 
