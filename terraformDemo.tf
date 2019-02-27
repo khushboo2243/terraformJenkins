94619a4a3e9129ffe5325ee0468ad31e091f84f4
Khushboo Nigam <khushboo2243@gmail.com>
2019-02-27 14:02:20 -0500

    Updating instance prinicipal

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -6,10 +6,8 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 provider "oci" {
-  tenancy_ocid = "${var.tenancy_ocid}"
-  user_ocid = "${var.user_ocid}"
-  fingerprint = "${var.fingerprint}"
-  private_key_path = "${var.private_key_path}"
+  
+ auth   = "InstancePrincipal"
   region = "${var.region}"
 }
 
