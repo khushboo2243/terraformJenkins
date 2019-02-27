0c7df2ec61328f082a6062fa98518cbf59d9d37b
Khushboo Nigam <khushboo2243@gmail.com>
2019-02-27 15:09:59 -0500

    Taking off the VCN block

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -13,9 +13,4 @@ provider "oci" {
   region = "${var.region}"
 }
 
-resource "oci_core_virtual_network" "VCN" {
-  cidr_block = "10.0.0.0/16"
-  dns_label = "VCN"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "NewVCN"
-}
+
