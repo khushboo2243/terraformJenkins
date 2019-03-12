712a2b2e8c0cf021f81d1a4e52db544ca27e9289
Khushboo Nigam <khushboo2243@gmail.com>
2019-03-12 13:22:30 -0400

    Taking off security rules and leaving only the table

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -42,52 +42,5 @@ resource "oci_core_route_table" "RouteTable" {
   vcn_id         = "${oci_core_virtual_network.VCN.id}"
   display_name   = "SecurityList"
 
-  // allow outbound tcp traffic on all ports
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "6"
-  }
-    
-    // allow outbound udp traffic on a port range
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "17"        // udp
-    stateless   = true
-
-    udp_options {
-      // These values correspond to the destination port range.
-      "min" = 319
-      "max" = 320
-    }
-    
-    // allow inbound ssh traffic from a specific port
-  ingress_security_rules {
-    protocol  = "6"         // tcp
-    source    = "0.0.0.0/0"
-    stateless = false
-
-    tcp_options {
-      source_port_range {
-        "min" = 100
-        "max" = 100
-      }
-
-      // These values correspond to the destination port range.
-      "min" = 22
-      "max" = 22
-    }
-  }
-    // allow inbound icmp traffic of a specific type
-  ingress_security_rules {
-    protocol  = 1
-    source    = "0.0.0.0/0"
-    stateless = true
-
-    icmp_options {
-      "type" = 3
-      "code" = 4
-    }
-  }
-  }
     }
   
