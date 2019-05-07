498eb4e104606c2921cd928cc5aaf9ad1f85d277
Khushboo Nigam <khushboo2243@gmail.com>
2019-05-07 15:40:42 -0400

    fixing compute file

--- compute.tf
+++ compute.tf
@@ -3,7 +3,7 @@
 resource "oci_core_instance" "Instance" {
   availability_domain = "LPEH:US-ASHBURN-AD-1"
   compartment_id      = "${var.compartment_ocid}"
-    image   = ${var.InstanceImageOCID[var.region]}
+    image   = "${var.InstanceImageOCID[var.region]}"
     shape = "${var.InstanceShape}"
     
 
@@ -18,5 +18,5 @@ resource "oci_core_instance" "Instance" {
     subnet_id        = "${oci_core_subnet.PublicSubnet.id}"
     assign_public_ip = true
   }
-  
+
 }
