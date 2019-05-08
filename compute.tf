bc9a32e0b2ac2d8ef0f28a2a0435248723a53924
Khushboo Nigam <khushboo2243@gmail.com>
2019-05-08 10:43:42 -0400

    changing provider

--- compute.tf
+++ compute.tf
@@ -15,6 +15,7 @@ resource "oci_core_instance" "Instance" {
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
+     
   }
 
   display_name = "Web-App-Instance"
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "oci" {
+tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu4es2i4x2xyeia"
+user_ocid="ocid1.user.oc1..aaaaaaaaxlahhu4jvjmnbo5rqnmgatmoqgvl4bugxox6jip2obep6kxn5u7q"
+compartment_ocid="ocid1.compartment.oc1..aaaaaaaaseo4aolyjd5ndfifkexmsu3bfweiko2to74lbru6x4djkmzz2d5q"
+fingerprint="a3:34:49:c9:10:24:71:8a:1e:e8:77:0d:4e:54:de:36"
+private_key_path="/Users/khnigam/.oci/oci_api_key.pem"
+region="us-ashburn-1"
+}
