4ca75b8b64eb6af62140f8a2a3ad93407884fd7c
Khushboo Nigam <khushboo2243@gmail.com>
2019-05-09 13:29:09 -0400

    check

--- provider.tf
+++ provider.tf
@@ -3,11 +3,10 @@ tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu
 user_ocid="ocid1.user.oc1..aaaaaaaaxlahhu4jvjmnbo5rqnmgatmoqgvl4bugxox6jip2obep6kxn5u7q"
 #compartment_ocid="ocid1.compartment.oc1..aaaaaaaaseo4aolyjd5ndfifkexmsu3bfweiko2to74lbru6x4djkmzz2d5q"
 fingerprint="41:5d:0f:a0:85:cf:f3:d6:3c:95:7b:c4:21:a3:a2:ca"
-private_key_path="/home/opc/.oci_kn/.oci_api_key.pem"
+private_key_path="/home/opc/.oci_kn/oci_api_key.pem"
 
 #fingerprint="a3:34:49:c9:10:24:71:8a:1e:e8:77:0d:4e:54:de:36"
 #private_key_path="/Users/khnigam/.oci_KN/oci_api_key.pem"
-
 region="us-ashburn-1"
 
 }
