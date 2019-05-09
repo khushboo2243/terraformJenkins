ecf962f54b4feacd47f69baeec87eaa7de1d2dfb
Khushboo Nigam <khushboo2243@gmail.com>
2019-05-09 11:57:18 -0400

    changing keys

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "oci" {
 tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaa5txfxrudc5ezitovlpjznw6b3l6sqvkzddepsfu4es2i4x2xyeia"
 user_ocid="ocid1.user.oc1..aaaaaaaaxlahhu4jvjmnbo5rqnmgatmoqgvl4bugxox6jip2obep6kxn5u7q"
 #compartment_ocid="ocid1.compartment.oc1..aaaaaaaaseo4aolyjd5ndfifkexmsu3bfweiko2to74lbru6x4djkmzz2d5q"
-fingerprint="a3:34:49:c9:10:24:71:8a:1e:e8:77:0d:4e:54:de:36"
-private_key_path="/Users/khnigam/.oci/oci_api_key.pem"
+fingerprint="41:5d:0f:a0:85:cf:f3:d6:3c:95:7b:c4:21:a3:a2:ca"
+private_key_path="/home/opc/.oci_kn/.oci_api_key.pem"
 region="us-ashburn-1"
 }
